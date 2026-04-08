99db783e3e74890f2bddb9dc2491d392fe879a4f
Decaxzm <barrfragan@gmail.com>
2026-04-07 21:50:53 -0400

    Arreglos conjuntos y despliegue en kubernetes

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ res.json({ message: 'Pedido registrado correctamente' });
 });
 
 
-app.listen(3000, () => {
-console.log('Servidor corriendo en http://localhost:3000');
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Servidor corriendo en puerto 3000');
 });
